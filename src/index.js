0d6b31ee0d5b52865a22e908fa5cf5a1b8bdb3ed
Jonathan Garcia <jona03g97@gmail.com>
2021-03-18 18:06:03 -0600

    Creamos conexión a servidor GraphQL

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Client from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import { App } from './App'
 
-render(<App />, document.getElementById('app'))
+const client = new Client({
+  uri: 'https://petgram-server-khaki-psi.vercel.app/graphql'
+})
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+)
